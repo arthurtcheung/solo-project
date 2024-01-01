a600189ad6f7354076705a03deb2d89ef25f8132
arthurtcheung <arthurtcheung>
2024-01-01 17:51:58 -0500

    Implemented react mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-
+import Map from 'react-map-gl';
 
 function App() {
   return (
-    <div className="App">
-      hello!
-    </div>
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} // NOTE: Mapbox access token stored in .env file
+      initialViewState={{
+        longitude: -73.9712,
+        latitude: 40.7831,
+        zoom: 14
+      }}
+      style={{width: '100vw', height: '100vh'}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
 
