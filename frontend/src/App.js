ed6dcca3f1935d06ff1efa0426f93936e70361fc
arthurtcheung <arthurtcheung>
2024-01-01 23:37:42 -0500

    Implemented marker and popup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,53 @@
-import Map from 'react-map-gl';
+import { useState } from 'react';
+import Map, { Marker, Popup } from 'react-map-gl'; // NOTE: react library; provides pre-built react components for mapbox api
+import PlaceIcon from '@mui/icons-material/Place'; // NOTE: react component library; includes pre-built components
+import StarIcon from '@mui/icons-material/Star';
+import './app.css';
 
 function App() {
+  // const [showPopup, setShowPopup] = useState<boolean>(true);
+
   return (
     <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} // NOTE: Mapbox access token stored in .env file
       initialViewState={{
-        longitude: -73.9712,
-        latitude: 40.7831,
-        zoom: 14
+        longitude: -95.8,
+        latitude: 40,
+        zoom: 3.8
       }}
-      style={{width: '100vw', height: '100vh'}}
+      style={ { width:'100vw', height:'100vh' } }
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+      mapboxAccessToken={ process.env.REACT_APP_MAPBOX_TOKEN }
+    >
+      <Marker 
+        longitude={ -73.97 } 
+        latitude={ 40.78 } color="red"
+      >
+        <PlaceIcon style={ {fontSize:40, color:'slateblue'} }/> 
+      </Marker>
+      <Popup 
+        longitude={ -73.97 } 
+        latitude={ 40.78 }
+        anchor="left"
+      >
+        <div className='popup'>
+          <label>Place</label>
+          <h4 className='place'>NYC</h4>
+          <label>Review</label>
+          <p className='review'>The best city.</p>
+          <label>Rating</label>
+            <div className='stars'>
+              <StarIcon className='stars' />
+              <StarIcon className='stars' />
+              <StarIcon className='stars' />
+              <StarIcon className='stars' />
+              <StarIcon className='stars' />
+            </div>
+          <label>Information</label>
+          <span className='username'>Created by <b>arthurtcheung</b></span>
+          <span className='date'>1 hour ago</span>
+        </div>
+      </Popup>
+    </Map>
   );
 }
 
